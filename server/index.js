abac90c4b8829eee7919353b299fe81df2394fa0
wincenzo2225-cloud <wincenzo22.25@gmail.com>
2026-01-23 03:17:08 +0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
-  res.send("Server isleyir");
+  res.send("Server işləyir");
 });
 
-const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log("Server acildi:", PORT);
+  console.log("Server started on " + PORT);
 });
